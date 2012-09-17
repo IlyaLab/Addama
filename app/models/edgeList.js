f444758bf68b1d80273c2cbf62d5c8bf63dca317
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-17 01:49:02 -0700

    try a sync override

--- app/models/edgeList.js
+++ app/models/edgeList.js
@@ -1,6 +1,26 @@
 var Collection = require('./collection');
-var Edge = require('./edge')
+var Edge = require('./edge');
 
 module.exports = Collection.extend({
-	model:Edge
+	model:Edge,
+
+	sync : function(method, model, options) {
+
+	  // var resp;
+	  // var store = model.localStorage || model.collection.localStorage;
+
+	  // switch (method) {
+	  //   case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
+	  //   case "create":  resp = store.create(model);                            break;
+	  //   case "update":  resp = store.update(model);                            break;
+	  //   case "delete":  resp = store.destroy(model);                           break;
+	  // }
+
+	  // if (resp) {
+	  //   options.success(resp);
+	  // } else {
+	  //   options.error("Record not found");
+	  // }
+	// };
+	}
 }) 
