0ceae2451f36d565248165b0f568766f059da778
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-17 01:46:58 -0700

    instantiate views inside controller or mediator events

--- app/application.js
+++ app/application.js
@@ -1,14 +1,13 @@
+var Controller = require('lib/controller');
+
 // Application bootstrapper.
 Application = {
   initialize: function() {
-    var HomeView = require('views/home_view');
-    var GraphView = require('views/graph_view');
     var Router = require('lib/router');
     // Ideally, initialized classes should be kept in controllers & mediator.
     // If you're making big webapp, here's more sophisticated skeleton
     // https://github.com/paulmillr/brunch-with-chaplin
-    this.homeView = new HomeView();
-    this.graphView = new GraphView();
+    this.Controller = Controller;
     this.router = new Router();
     if (typeof Object.freeze === 'function') Object.freeze(this);
   }
