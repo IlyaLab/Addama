7d39a2655b58c73e3b42382d5a8d589476d6ce21
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-19 12:47:58 -0700

    do not explicitly navigate. let backbone do its thing

--- app/views/topbar_view.js
+++ app/views/topbar_view.js
@@ -5,14 +5,6 @@ module.exports = View.extend({
   id: 'top-bar',
   template: template,
 
-    events:{
-        'click .dropdown-menu a':function(source) {
-            var hrefRslt = source.target.getAttribute('href');
-            Backbone.history.navigate(hrefRslt, {trigger:true});
-            //Cancel the regular event handling so that we won't actual change URLs
-            //We are letting Backbone handle routing
-            return false;
-        }
-    }
+    
 
 });
