8bf32183643545c37d597b6c139a1aab25e8df4d
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-20 17:53:20 -0700

    graph retrieves and parses models. feeds nodes and edges to correct models

--- app/models/featureList.js
+++ app/models/featureList.js
@@ -1,32 +1,7 @@
 var Collection = require('./collection');
-var Feature = require('./feature')
+var Feature = require('./feature');
 
 module.exports = Collection.extend({
-	model:Feature,
-
-	url : function() {
-		return this.websvc + this.feature_list.join(',');
-	},
-
-	initialize: function(options) {
-		options || (options = {});
-		this.feature_list = options.feature_list;
-		this.websvc = options.websvc;
-
-	},
-
-	parse: function(response){
-		var response_array = response.split('\n');
-		var headers = response_array[0].split('\t');
-		var feature_array = _rest(response_array,1);
-		var features = feature_array.map(function(f) { 
-				var cols = f.split('\t');
-				return { 
-						label : cols[0],
-						values : _rest(cols,1)
-					}
-			});
-
-	}
+	model: Feature
 
 });
--- app/models/graph.js
+++ app/models/graph.js
@@ -1,19 +1,84 @@
-var Model = require('./model')
-var FeatureList = require('./featureList')
-var EdgeList = require('./edgeList')
+var Model = require('./dataset');
+var FeatureList = require('./featureList');
+var EdgeList = require('./edgeList');
 
 module.exports = Model.extend({
 
+	serviceRoot : '/endpoints',
+	serviceRead : '/read_file?',
+	analysisParam :'filepath=',
 
-  initialize: function(){
-  	this.nodes = new FeatureList
-  	this.edges = new EdgeList
+	defaults: {
+		edges : new EdgeList(),
+		nodes : new FeatureList()
+	},
 
-  },
+	initialize: function() {
+		_.bindAll(this,'getEdgesArray','getNodesArray','url','parse','defaultParameters');
+	},
 
-  sync : function(method, model) {
-  	this.nodes.reset(graph.nByi);
-  	this.edges.reset(graph.adj); 
-  }
+	getEdgesArray : function() {
+			return this.get('edges').toJSON();
+	},
+
+	getNodesArray : function() {
+			return this.get('nodes').toJSON();
+	},
+
+	defaultParameters : function() {
+			var options = new Object();
+
+			switch ( this.get('analysis_id') ) {
+				case('pwpv') :
+					_.extend(options, {x:'r1',y:'r2',edgeRouting:'straight'});
+				break;
+				case('rf_ace') :
+				default:
+					_.extend(options, {x:'r1',y:'hodge',edgeRouting:'diagonal'});
+				break;
+			}
+
+			return options;
+	},
+
+	url : function() {
+		return this.serviceRoot + this.serviceRead+ this.analysisParam + encodeURIComponent('/'+ this.get('analysis_id') + '/' + this.get('dataset_id'));
+	},
+
+	parse: function(graphData){
+
+		var node_data = new Object(),
+				nodes = new Array(),
+			    edges = graphData.adj,
+			data_keys = new Array(),
+	              obj = new Object(),
+	                i = 0;
+ 		    
+	    Object.keys(graphData).forEach(function(a) {  // strip out edges
+	        if (a === 'adj' || a ==='iByn')   return;
+	        node_data[a] = _.clone(graphData[a]);
+	      });
+
+	    node_data.label = node_data.nByi.map(function(f) { return f.split(':')[2];});
+
+	    data_keys = _.keys(node_data);
+	    key_len = data_keys.length;
+	    
+	      //keys of the data structure    
+	    nodes = _.range(node_data[data_keys[0]].length)
+	             .map(function(row) {
+	                    obj={};
+	                    for (i=0; i < key_len; i++)  {
+	                    	obj[data_keys[i]] = node_data[data_keys[i]][row];
+	                    }
+	                    return obj;
+	            });  
+
+	    edges = edges.map(function(edge) { return { source:edge[1], target:edge[0],weight:edge[2]}; } ); 
+
+			//the returned object is mapped to the model properties ie. this.nodes, this.edges
+		return { nodes: new FeatureList(nodes), edges : new EdgeList(edges) };  
+
+	}
 
 });
