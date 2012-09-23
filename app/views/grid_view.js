a5b14bf9947ff6228438f67b972d88aab8f0b22a
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-23 03:20:17 -0700

    improve grid

--- app/views/grid_view.js
+++ app/views/grid_view.js
@@ -22,18 +22,55 @@ module.exports = View.extend({
   renderGrid : function(){
 	var container = this.$el.find(".grid-container");
 		var grid,
-		    columns = [];
+		    columns = [],
+		    rows = this.model.toJSON();
 
 		_.each(this.model.getHeaders(), function(i) {
-			columns.push({id: i, name: i, field: i, sortable: true});
+			columns.push({id: i, name: i, field: i, sortable: true, selectable: true});
 		});
+
+	    var checkboxSelector = new Slick.CheckboxSelectColumn({
+	      cssClass: "slick-cell-checkboxsel"
+	    });
+
+	    columns.unshift(checkboxSelector.getColumnDefinition());
 
 		var options = {
 			enableCellNavigation: false,
 			enableColumnReorder: false
 		};
-		grid = new Slick.Grid(container, this.model.toJSON(), columns, options);
-		
+
+		grid = new Slick.Grid(container, rows, columns, options);
+		grid.setSelectionModel(new Slick.RowSelectionModel({selectActiveRow: false}));
+		grid.registerPlugin(checkboxSelector);
+		grid.onSort.subscribe(function(e, args){ // args: sort information. 
+
+        var field = args.sortCol.field;
+
+        rows.sort(function(a, b){
+        	var x = a[field],
+        	    y = b[field],
+        	    result;
+
+        	    if (!(_.isNumber(x) || _.isNumber(y))) {
+        	    		result = 
+        	    			a[field] > b[field] ? 1 :
+			                a[field] < b[field] ? -1 :
+			                0
+			                ;
+        	    } else if (!_.isNumber(x)) {
+        	    		result = 1;
+        	    } else if (!_.isNumber(y)) {
+        	    		result = -1;
+        	    } else {
+			            result = x-y;
+			        }
+            return args.sortAsc ? result : -result;
+        });
+        
+        args.grid.invalidateAllRows();
+   		args.grid.render();
+    });
 	}
 
 });
