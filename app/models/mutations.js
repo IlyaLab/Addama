8fe325af4762dad8e6d8d9318ad25e6ebf115a81
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-24 06:47:35 -0700

    add models for info gain and mutations

---
+++ app/models/mutations.js
@@ -0,0 +1,13 @@
+var Model = require('./model');
+
+module.exports = Model.extend({
+
+	serviceRoot : '/svc',
+	serviceRead : '/data',
+	serviceDir :'/analysis/mutations',
+
+	url : function() {
+		return this.serviceRoot + this.serviceRead + this.serviceDir + '/' + this.get('dataset_id');
+	}
+
+});
