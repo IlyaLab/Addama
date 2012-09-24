02e8f16451bf82f513dd2718b089f0ed8650c89e
Hector Rovira <hrovira@systemsbiology.org>
2012-09-24 13:43:08 -0700

    stubbed out controls

--- app/views/graph_view.js
+++ app/views/graph_view.js
@@ -6,66 +6,135 @@ var PC = require('./parcoords_view');
 
 module.exports = View.extend({
 
-  model : Graph,
-  template:template,
+    model:Graph,
+    template:template,
 
-  initialize : function() {
-      _.bindAll(this,'afterRender','renderGraph');
-  },
-  
-  afterRender: function() {
-    var _this = this;
-    this.$el.addClass('row-fluid');
-    this.model.fetch().done(_this.renderGraph);
-  },
+    events: {
+        "click .graph-controls-reset": "resetControls",
+        "click .toggle-labels .toggle-on": "showLabels",
+        "click .toggle-labels .toggle-off": "hideLabels",
+        "click .toggle-lines .toggle-on": "showLines",
+        "click .toggle-lines .toggle-off": "hideLines",
+        "click .toggle-on, .toggle-off": "toggleActive",
+        "click #dl-straight, #dl-diagonal, #dl-diagonal-directed": "toggleDynLayout",
+        "click #dl-straight": "layoutStraight",
+        "click #dl-diagonal": "layoutDiagonal",
+        "click #dl-diagonal-directed": "layoutDiagonalDirected"
+    },
 
-  renderGraph: function(options) {
-      var _this = this;
-      var parentDiv = this.$el.find('.graph-container'),
-      w= parentDiv.width(),
-      h= parentDiv.height();
-      var vis_options = this.model.defaultParameters();
-      
-      var x = vis_options.x || 'r1',
-       y = vis_options.y || 'hodge',
-       edgeRouting = vis_options.edgeRouting || 'straight';
+    initialize:function () {
+        _.bindAll(this, 'afterRender', 'renderGraph', 'wireControls', 'resetControls',
+                    'showLabels', 'hideLabels', 'showLines', 'hideLines', 'toggleActive',
+                    'layoutStraight', 'layoutDiagonal', 'layoutDiagonalDirected');
+    },
 
-      var edge_scale = d3.scale.log().domain(d3.extent(_this.model.getEdgesArray(),function(a) { return a[2];})).range([0.2,1.0]);
-      var edgeO = function(edge) {
-          return edge_scale(edge.weight);
-      };
+    afterRender:function () {
+        var _this = this;
+        this.$el.addClass('row-fluid');
+        this.model.fetch().done(_this.renderGraph);
+    },
 
-  		var treeChart = TreeChart({
-                          			width:w, 
-                          			height:h,   			
-                          			nodes:{
-                                    data : this.model.getNodesArray(),
-                          					y: y,
-                          					x: x
-                          			},
-                                edges: {
-                                    data: this.model.getEdgesArray()
-                                }
-                		  })
-                    .edgeOpacity(edgeO)
-                    .edgeRoute(edgeRouting);
+    renderGraph:function (options) {
+        console.log("renderGraph");
 
-  		d3.select('.graph-container')
-              .call(treeChart);
+        var _this = this;
+        var parentDiv = this.$el.find('.graph-container'),
+            w = parentDiv.width(),
+            h = parentDiv.height();
+        var vis_options = this.model.defaultParameters();
 
-      var filter = this.$el.find('.filter-container');
-      var pc_view = new PC({model:this.model});
-      filter.html(pc_view.render().el);
-      pc_view.showData();
+        var x = vis_options.x || 'r1',
+            y = vis_options.y || 'hodge',
+            edgeRouting = vis_options.edgeRouting || 'straight';
 
-      Backbone.Mediator.subscribe('dimension:select',dimension_selected, this, false );
+        var edge_scale = d3.scale.log().domain(d3.extent(_this.model.getEdgesArray(), function (a) {
+            return a[2];
+        })).range([0.2, 1.0]);
+        var edgeO = function (edge) {
+            return edge_scale(edge.weight);
+        };
 
-      function dimension_selected(dimension) {
-        var scale = d3.scale.linear().domain(d3.extent(graphData[dimension])).range([0.1,1.0]);
-        treeChart.nodeOpacity(function(node) { return scale(node[dimension]);})
-        treeChart.redraw();
-      }
-     
-  }
+        var treeChart = TreeChart({
+            width:w,
+            height:h,
+            nodes:{
+                data:this.model.getNodesArray(),
+                y:y,
+                x:x
+            },
+            edges:{
+                data:this.model.getEdgesArray()
+            }
+        })
+            .edgeOpacity(edgeO)
+            .edgeRoute(edgeRouting);
+
+        d3.select('.graph-container')
+            .call(treeChart);
+
+        var filter = this.$el.find('.filter-container');
+        var pc_view = new PC({model:this.model});
+        filter.html(pc_view.render().el);
+        pc_view.showData();
+
+        Backbone.Mediator.subscribe('dimension:select', dimension_selected, this, false);
+
+        function dimension_selected(dimension) {
+            var scale = d3.scale.linear().domain(d3.extent(graphData[dimension])).range([0.1, 1.0]);
+            treeChart.nodeOpacity(function (node) {
+                return scale(node[dimension]);
+            });
+            treeChart.redraw();
+        }
+
+    },
+
+    wireControls:function () {
+        console.log("wireControls");
+    },
+
+    showLabels: function(ev) {
+        console.log("showLabels");
+    },
+
+    hideLabels: function(ev) {
+        console.log("hideLabels");
+    },
+
+    showLines: function() {
+        console.log("showLines");
+    },
+
+    hideLines: function() {
+        console.log("hideLines");
+    },
+
+    resetControls: function() {
+        console.log("resetControls");
+    },
+
+    toggleActive: function(ev) {
+        var el = ev.target;
+        if (!$(el).hasClass("active")) {
+            $(el.parentNode).find("button").toggleClass("active");
+        }
+    },
+
+    toggleDynLayout: function(ev) {
+        $(".dynamic-layout .active").removeClass("active");
+        $(ev.target).addClass("active");
+    },
+
+    layoutStraight: function(ev) {
+        console.log("layoutStraight");
+    },
+
+    layoutDiagonal: function(ev) {
+        console.log("layoutDiagonal");
+    },
+
+    layoutDiagonalDirected: function(ev) {
+        console.log("layoutDiagonalDirected");
+    }
 
 });
