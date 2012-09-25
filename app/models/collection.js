6cf2f4577aafd960f5b10d9efd3c68df222b5841
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-24 21:14:12 -0700

    add original version for filtering

--- app/models/collection.js
+++ app/models/collection.js
@@ -1,4 +1,11 @@
 // Base class for all collections.
 module.exports = Backbone.Collection.extend({
   
+  original_collection : new Backbone.Collection(),
+
+original: function(value) {
+  		if (!arguments.length) return this.original_collection;
+		    this.original_collection = value;
+	    return this;
+	}
 });
--- app/models/model.js
+++ app/models/model.js
@@ -1,4 +1,11 @@
 // Base class for all models.
 module.exports = Backbone.Model.extend({
 
+  original_model : new Backbone.Model(),
+
+original: function(value) {
+  		if (!arguments.length) return this.original_collection;
+		    this.original_collection = value;
+	    return this;
+	}
 });
