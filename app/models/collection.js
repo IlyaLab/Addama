200036b2a90b1ae8957d1a1c8d810fd34bc42202
Dick Kreisberg <rbkreisberg@gmail.com>
2012-09-27 15:42:59 -0700

    formatting

--- app/models/collection.js
+++ app/models/collection.js
@@ -1,11 +1,12 @@
 // Base class for all collections.
 module.exports = Backbone.Collection.extend({
   
-  original_collection : new Backbone.Collection(),
+  original_collection : Backbone.Collection,
 
-original: function(value) {
+  original: function(value) {
   		if (!arguments.length) return this.original_collection;
 		    this.original_collection = value;
 	    return this;
 	}
+
 });
