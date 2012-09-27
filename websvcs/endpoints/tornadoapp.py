d16fc81d050d526a75e06bf9bbcbed014d593603
Hector Rovira <hrovira@systemsbiology.org>
2012-09-27 15:45:57 -0700

    adding oauth to google

--- websvcs/endpoints/tornadoapp.py
+++ websvcs/endpoints/tornadoapp.py
@@ -23,6 +23,7 @@ All services return -1 if there is any error.
 import tornado.ioloop
 from tornado.options import define, options, logging
 import tornado.web
+import tornado.auth
 import qedconf
 import os
 
@@ -112,8 +113,21 @@ class FilterHandler(tornado.web.RequestHandler):
         except:
             self.write("-1")
 
+class GoogleHandler(tornado.web.RequestHandler, tornado.auth.GoogleMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        print "GoogleHandler.get"
+        if self.get_argument("openid.mode", None):
+            self.get_authenticated_user(self.async_callback(self._on_auth))
+            return
+        self.authenticate_redirect(callback_uri="/svc/login")
 
-
+    def _on_auth(self, user):
+        print "GoogleHandler._on_auth(%s)" % user
+        if not user:
+            raise tornado.web.HTTPError(500, "Google auth failed")
+        # Save the user with, e.g., set_secure_cookie()
+        self.redirect("/")
 
 def main():
     tornado.options.parse_command_line()
@@ -121,6 +135,7 @@ def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/data?(.*)", FilterHandler),
+        (r"/login", GoogleHandler)
     ], **settings)
     application.listen(options.port, **server_settings)
     tornado.ioloop.IOLoop.instance().start()
