d19920814213f541230654128d1c119783890870
Hector Rovira <hrovira@systemsbiology.org>
2012-10-04 11:30:00 -0700

    repackaged storage

--- websvcs/endpoints/tornadoapp.py
+++ websvcs/endpoints/tornadoapp.py
@@ -23,14 +23,10 @@ All services return status_code 500 if there is any error.
 import tornado.ioloop
 from tornado.options import define, options, logging
 import tornado.web
-import tornado.auth
-import tornado.httpclient
-import os
 import json
-import uuid
-import glob
 
 from data import FilterHandler
+from storage import StorageHandler
 from oauth import GoogleOAuth2Handler, GoogleSignoutHandler
 
 define("data_path", default="../..", help="Path to data files")
@@ -85,50 +81,6 @@ class WhoamiHandler(tornado.web.RequestHandler):
         providers.append({ "id": "twitter", "label": "Twitter", "active": False, "logo":"https://twitter.com/images/resources/twitter-bird-white-on-blue.png" })
 
         self.write({"providers":providers});
-        self.set_status(200)
-
-class StorageHandler(tornado.web.RequestHandler):
-    def get(self, identity):
-        ids = identity.split("/")
-        if len(ids) == 1:
-            json_items = []
-            for item in glob.glob("STORAGE/%s/*.dat" % ids[0]):
-                item_id = item.replace("STORAGE/%s/" % ids[0], "").replace(".dat", "")
-                json_items.append({
-                    "id": item_id,
-                    "uri": self.request.uri + "/" + item_id
-                })
-            self.write({ "items": json_items })
-            self.set_status(200)
-            return
-        elif len(ids) == 2:
-            file_path = "STORAGE/%s/%s.dat" % (ids[0],ids[1])
-            if os.path.exists(file_path):
-                json_data=open(file_path).read()
-                self.write(json.loads(json_data))
-                self.set_status(200)
-                return
-            else:
-                self.set_status(404)
-                return
-
-        self.set_status(404)
-
-    def post(self, identity):
-        ids = identity.split("/")
-
-        item_id = None
-        if len(ids) == 1: item_id = uuid.uuid4()
-        elif len(ids) == 2: item_id = ids[1]
-        else: return
-
-        if not os.path.exists("STORAGE"): os.makedirs("STORAGE")
-        if not os.path.exists("STORAGE/" + ids[0]): os.makedirs("STORAGE/" + ids[0])
-
-        f = open('STORAGE/%s/%s.dat' % (ids[0], item_id), 'w+')
-        f.write(json.dumps(self.request.arguments))
-
-        self.write({ "id": str(item_id) })
         self.set_status(200)
 
 def main():
