784aafad481aeafbcc12276cf6d24e0b697a1350
Hector Rovira <hrovira@systemsbiology.org>
2012-10-04 11:25:38 -0700

    repackaged data fetch against local files

--- websvcs/endpoints/tornadoapp.py
+++ websvcs/endpoints/tornadoapp.py
@@ -30,9 +30,10 @@ import json
 import uuid
 import glob
 
+from data import FilterHandler
 from oauth import GoogleOAuth2Handler, GoogleSignoutHandler
 
-define("data_path", default="../../", help="Path to data files")
+define("data_path", default="../..", help="Path to data files")
 define("port", default=8000, help="run on the given port", type=int)
 define("client_host", default="http://localhost:8000", help="Client URL for Google OAuth2")
 define("client_id", help="Client ID for Google OAuth2")
@@ -59,65 +60,6 @@ def _writeFilteredRow(self,line,cols):
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Service Root")
-
-
-class FilterHandler(tornado.web.RequestHandler):
-    def get(self,filepath):
-        self.content_type = "text/plain"
-        try:
-            rows=self.get_arguments("rows")
-            if len(rows) > 0: 
-                rows = rows[0].split(",")
-            cols=self.get_arguments("cols")
-            if len(cols) > 0:
-                cols = frozenset(cols[0].split(","))
-            
-            goodcols=[0]
-            
-            if len(rows) > 0 or len(cols)>0: 
-                
-
-                rfile = open(options.data_path + filepath)
-                for idx, line in enumerate(rfile):
-                    if idx == 0:
-                        colhead = line.rstrip("\n\r").split()
-                        if len(cols) > 0:
-                            for i,h in enumerate(colhead):
-                                if h in cols:
-                                    goodcols.append(i)
-                        goodcols=frozenset(goodcols)
-                        _writeFilteredRow(self,line,goodcols)
-
-                        
-                    elif len(rows)==0:
-                        _writeFilteredRow(self,line,goodcols)
-                    else:
-                        rheaader = line[:line.rfind("\t")]
-                        for id in rows:
-                            if id in rheaader:
-                                _writeFilteredRow(self, line, goodcols)
-                                break
-            elif os.path.isdir(options.data_path + filepath):
-                dirs=[]
-                files=[]
-                for f in os.listdir(options.data_path + filepath):
-                    if not f.startswith("."):
-                        label = os.path.basename(f)
-                        item = '{"label":"%s", "uri":"%s"}' % (label, os.path.join(filepath,f))
-
-                        if os.path.isdir(options.data_path + filepath + f):
-                            dirs.append(item)
-                        else:
-                            files.append(item)
-
-                self.write('{ "directories": [' + ",".join(dirs) + '], "files": [' + ",".join(files) + '] }')
-            else:
-                rfile = open(options.data_path + filepath)
-                self.write(rfile.read())
-                rfile.close()
-
-        except:
-            self.set_status(500)
 
 class WhoamiHandler(tornado.web.RequestHandler):
     def get(self):
