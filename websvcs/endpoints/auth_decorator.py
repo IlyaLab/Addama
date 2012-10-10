6485072db97dec5a1cce4343dcbaacd36ecd43cc
Hector Rovira <hrovira@systemsbiology.org>
2012-10-10 10:00:58 -0700

    allow public access if no authorized_users specified in config file

--- websvcs/endpoints/auth_decorator.py
+++ websvcs/endpoints/auth_decorator.py
@@ -6,12 +6,13 @@ def authenticated(method):
     """Decorate methods with this to require that the user be logged in."""
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        whoami = self.get_cookie("whoami")
-        if not whoami:
-            raise tornado.web.HTTPError(403)
+        if len(options.authorized_users) > 0:
+            whoami = self.get_cookie("whoami")
+            if not whoami:
+                raise tornado.web.HTTPError(403)
 
-        if not whoami in options.authorized_users:
-            raise tornado.web.HTTPError(403)
+            if not whoami in options.authorized_users:
+                raise tornado.web.HTTPError(403)
                     
         return method(self, *args, **kwargs)
     return wrapper
