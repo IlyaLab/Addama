2c80afbefbaebb9e77beb4bb71859788b6f70647
Hector Rovira <hrovira@systemsbiology.org>
2012-10-17 12:04:29 -0700

    added utility functions

--- app/models/model.js
+++ app/models/model.js
@@ -1,11 +1,20 @@
 // Base class for all models.
 module.exports = Backbone.Model.extend({
 
-  original_model : new Backbone.Model(),
+    original_model:new Backbone.Model(),
 
-original: function(value) {
-  		if (!arguments.length) return this.original_collection;
-		    this.original_collection = value;
-	    return this;
-	}
+    original:function (value) {
+        if (!arguments.length) return this.original_collection;
+        this.original_collection = value;
+        return this;
+    },
+
+    standard_fetch:function () {
+        this.fetch({
+            success:function (m) {
+                m.trigger('load');
+            }
+        });
+        return this;
+    }
 });
--- app/views/view.js
+++ app/views/view.js
@@ -2,22 +2,36 @@ require('lib/view_helper');
 
 // Base class for all views.
 module.exports = Backbone.View.extend({
-  initialize: function() {
-    this.render = _.bind(this.render, this);
-  },
+    initialize:function () {
+        this.render = _.bind(this.render, this);
+    },
 
-  template: function() {},
-  getRenderData: function() {},
+    template:function () {
+    },
+    getRenderData:function () {
+    },
 
-  render: function() {
-    this.$el.html(this.template(this.getRenderData()));
-    this.afterRender();
-    return this;
-  },
+    render:function () {
+        this.$el.html(this.template(this.getRenderData()));
+        this.afterRender();
+        return this;
+    },
 
-    refresh: function() {
+    refresh:function () {
         this.$el.html(this.template(this.getRenderData()));
     },
 
-  afterRender: function() {}
+    afterRender:function () {
+    },
+
+    multiLoad:function (models, callback) {
+        if (models && models.length && callback) {
+            console.log("multiLoad [" + models.length + "]");
+            callback = _.after(models.length, callback);
+
+            _.each(models, function (model) {
+                model.on("load", callback);
+            });
+        }
+    }
 });
