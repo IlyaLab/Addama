986fcd37e6f6e07a4bccb2de2ab371eb01be88f8
Hector Rovira <hrovira@systemsbiology.org>
2012-11-08 13:51:34 -0800

    working prototype state is good

--- vendor/js/datamap.js
+++ vendor/js/datamap.js
@@ -1,5 +1,3 @@
-// TODO : Figure out better way to get native element types (e.g. this.$el.dataset)
-// TODO : Canvas may be required
 !function ($) {
     var DataMap = function (element) {
         this.$el = $(element);
@@ -24,33 +22,32 @@
             _.bindAll(this, "_initStep", "_computeWindowScale", "_perspective", "_scale", "_translate", "_rotate");
             _.bindAll(this, "_onStepEnter", "_onStepLeave");
 
-            var rootData = this.$el[0].dataset;
-            var config = {
-                width:(rootData.width || this.width),
-                height:(rootData.height || this.height),
-                maxScale:(rootData.maxScale || this.maxScale),
-                minScale:(rootData.minScale || this.minScale),
-                perspective:(rootData.perspective || this.perspective),
-                transitionDuration:(rootData.transitionDuration || this.transitionDuration)
-            };
-
-            this.windowScale = this._computeWindowScale(config);
+            this.windowScale = this._computeWindowScale();
 
             this.$el.css({
+//                height: "90%",
+                border:"1px solid black",
+                overflow:"hidden"
+            });
+
+            var rootStyles = {
                 position:"absolute",
                 transformOrigin:"top left",
                 transition:"all 0s ease-in-out",
-                transformStyle:"preserve-3d",
+                transformStyle:"preserve-3d"
+            };
+
+            this.$canvas = this.$el.find(".impress-canvas");
+            this.$canvas.css(rootStyles);
+
+            this.$stepWrap = this.$canvas.find("#impress");
+            this.$stepWrap.css(_.extend(rootStyles, {
                 top:"50%",
                 left:"50%",
-                transform:this._perspective(config.perspective / this.windowScale) + this._scale(this.windowScale)
-            });
+                transform:this._perspective(this.perspective / this.windowScale) + this._scale(this.windowScale)
+            }));
 
-            this.$el.removeClass("impress-disabled");
-            this.$el.addClass("impress-enabled");
-
-            // get and init steps
-            _.each(this.$el.find(".step"), this._initStep);
+            _.each(this.$stepWrap.find(".step"), this._initStep);
 
             // set a default initial state of the canvas
             this.currentState = {
@@ -59,17 +56,15 @@
                 scale:1
             };
 
-            initialized = true;
-
             this.$el.trigger("impress:init", { api:this });
         },
 
         goto:function (options) {
-            console.log("DataMap.goto:" + options.step);
             var $step = $(options.step);
             var stepId = $step[0].id;
+            console.log("DataMap.goto:" + stepId);
 
-            window.scrollTo(0, 0);
+//            window.scrollTo(0, 0);
 
             if (this.activeStep) {
                 this.activeStep.removeClass("impress-active");
@@ -109,7 +104,7 @@
             // if the same step is re-selected, force computing window scaling,
             // because it is likely to be caused by window resize
             if ($step === this.activeStep) {
-                this.windowScale = computeWindowScale(config);
+                this.windowScale = this._computeWindowScale();
             }
 
             var targetScale = target.scale * this.windowScale;
@@ -127,7 +122,7 @@
             // Transitions on them are triggered with different delays (to make
             // visually nice and 'natural' looking transitions), so we need to know
             // that both of them are finished.
-            this.$el.css({
+            this.$stepWrap.css({
                 // to keep the perspective look similar for different scales
                 // we need to 'scale' the perspective, too
                 transform:this._perspective(this.perspective / targetScale) + this._scale(targetScale),
@@ -135,21 +130,12 @@
                 transitionDelay:(zoomin ? delay : 0) + "ms"
             });
 
-//            this.$canvas.css({
-//                transform:rotate(target.rotate, true) + translate(target.translate),
-//                transitionDuration:duration + "ms",
-//                transitionDelay:(zoomin ? 0 : delay) + "ms"
-//            });
+            this.$canvas.css({
+                transform:this._rotate(target.rotate, true) + this._translate(target.translate),
+                transitionDuration:duration + "ms",
+                transitionDelay:(zoomin ? 0 : delay) + "ms"
+            });
 
-            // Here is a tricky part...
-            //
-            // If there is no change in scale or no change in rotation and translation, it means there was actually
-            // no delay - because there was no transition on `root` or `canvas` elements.
-            // We want to trigger `impress:stepenter` event in the correct moment, so here we compare the current
-            // and target values to check if delay should be taken into account.
-            //
-            // I know that this `if` statement looks scary, but it's pretty simple when you know what is going on
-            // - it's simply comparing all the values.
             if (this.currentState.scale === target.scale ||
                 (this.currentState.rotate.x === target.rotate.x && this.currentState.rotate.y === target.rotate.y &&
                     this.currentState.rotate.z === target.rotate.z && this.currentState.translate.x === target.translate.x &&
@@ -174,7 +160,7 @@
             // If you want learn something interesting and see how it was done with `transitionend` go back to
             // version 0.5.2 of impress.js: http://github.com/bartaz/impress.js/blob/0.5.2/js/impress.js
             var _this = this;
-            _.defer(function() {
+            _.defer(function () {
                 _this._onStepEnter(_this.activeStep);
             });
         },
@@ -188,21 +174,21 @@
         },
 
         _initStep:function (el, idx) {
-            console.log("_initStep:" + el.id + "," + idx);
+            var $stepEl = $(el);
             if (!el.id) el.id = "step-" + (idx + 1);
 
             var step = {
                 translate:{
-                    x:(el.dataset.x),
-                    y:(el.dataset.y),
-                    z:(el.dataset.z)
+                    x:($stepEl.data("x")),
+                    y:($stepEl.data("y")),
+                    z:($stepEl.data("z"))
                 },
                 rotate:{
-                    x:(el.dataset.rotateX),
-                    y:(el.dataset.rotateY),
-                    z:(el.dataset.rotateZ || el.dataset.rotate)
+                    x:($stepEl.data("rotateX")),
+                    y:($stepEl.data("rotateY")),
+                    z:($stepEl.data("rotateZ") || $stepEl.data("rotate"))
                 },
-                scale:(el.dataset.scale || 1),
+                scale:($stepEl.data("scale") || 1),
                 el:el
             };
 
@@ -215,16 +201,16 @@
             });
         },
 
-        _computeWindowScale:function (config) {
-            var hScale = window.innerHeight / config.height;
-            var wScale = window.innerWidth / config.width;
+        _computeWindowScale:function () {
+            var hScale = this.height;
+            var wScale = this.width;
             var scale = hScale > wScale ? wScale : hScale;
 
-            if (config.maxScale && scale > config.maxScale) {
-                scale = config.maxScale;
+            if (this.maxScale && scale > this.maxScale) {
+                scale = this.maxScale;
             }
-            if (config.minScale && scale < config.minScale) {
-                scale = config.minScale;
+            if (this.minScale && scale < this.minScale) {
+                scale = this.minScale;
             }
             return scale;
         },
@@ -242,23 +228,22 @@
         },
 
         _rotate:function (r, revert) {
-            var rX = " rotateX(" + r.x + "deg) ",
-                rY = " rotateY(" + r.y + "deg) ",
-                rZ = " rotateZ(" + r.z + "deg) ";
-
+            var rX = " rotateX(" + r.x + "deg) ";
+            var rY = " rotateY(" + r.y + "deg) ";
+            var rZ = " rotateZ(" + r.z + "deg) ";
             return revert ? rZ + rY + rX : rX + rY + rZ;
         },
 
-        _onStepEnter: function(step) {
+        _onStepEnter:function (step) {
             if (this.lastEntered !== step) {
-                this.$el.trigger("impress:stepenter", { "step": step });
+                this.$el.trigger("impress:stepenter", { "step":step });
                 this.lastEntered = step;
             }
         },
 
-        _onStepLeave: function(step) {
+        _onStepLeave:function (step) {
             if (this.lastEntered === step) {
-                this.$el.trigger("impress:stepleave", { "step": step });
+                this.$el.trigger("impress:stepleave", { "step":step });
                 this.lastEntered = null;
             }
         }
