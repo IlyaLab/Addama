686b5a060a1d3bfb3dcc5148d2ca8ad901053c1c
Hector Rovira <hrovira@systemsbiology.org>
2012-11-12 23:07:43 -0800

    laying out elements in atlas

--- app/lib/atlas.js
+++ app/lib/atlas.js
@@ -1,4 +1,5 @@
 var QEDRouter = require("./router");
+var LineItemTemplate = require("../views/templates/line_item");
 
 // TODO : Pass genes and cancers as view options
 // TODO :
@@ -8,6 +9,10 @@ module.exports = Backbone.View.extend({
         this.$el = $(element);
         if (options) _.extend(this, options);
         _.bindAll(this, "emphasize", "maximize", "minimize", "minimize_all", "navigate");
+        _.bindAll(this, "loadCancerList", "initGeneTypeahead");
+
+        $.ajax({ url:"svc/data/lookups/cancers", type:"GET", dataType:"text", success:this.loadCancerList });
+        $.ajax({ url:"svc/data/lookups/genes", type:"GET", dataType:"text", success:this.initGeneTypeahead });
     },
 
     emphasize: function(options) {
@@ -60,5 +65,44 @@ module.exports = Backbone.View.extend({
             var router = new QEDRouter({"targetEl": $(target)});
             router.viewsByUri(uri, view_name);
         }
+    },
+
+    loadCancerList: function(txt) {
+        var cancerList = txt.trim().split("\n");
+        var UL = $(".cancer-selector");
+        _.each(cancerList, function(cancerItem) {
+            UL.append(LineItemTemplate({"li_class":"active","a_class":"toggle-active","id":cancerItem,"label":cancerItem}));
+        });
+
+        UL.find(".toggle-active").click(function(e) {
+            $(e.target).parent().toggleClass("active");
+        });
+    },
+
+    initGeneTypeahead: function(txt) {
+        var genelist = txt.trim().split("\n");
+
+        var UL = $(".gene-selector");
+        $(".genes-typeahead").typeahead({
+            source:function (q, p) {
+                p(_.compact(_.flatten(_.map(q.toLowerCase().split(" "), function (qi) {
+                    return _.map(genelist, function (geneitem) {
+                        if (geneitem.toLowerCase().indexOf(qi) >= 0) return geneitem;
+                    });
+                }))));
+            },
+
+            updater:function (gene) {
+                UL.append(LineItemTemplate({ "label":gene, "id":gene, "a_class":"item-remover", "i_class":"icon-trash" }));
+                UL.find(".item-remover").click(function(e) {
+                    $(e.target).parent().remove();
+                });
+                return gene;
+            }
+        });
+
+        UL.find(".item-remover").click(function(e) {
+            $(e.target).parent().remove();
+        });
     }
 });
