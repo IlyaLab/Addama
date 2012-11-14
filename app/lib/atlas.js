bcf152c11afe62eb1df6249454f0b2c989c99143
Hector Rovira <hrovira@systemsbiology.org>
2012-11-14 15:54:12 -0800

    refactored zoom layout

--- app/lib/atlas.js
+++ app/lib/atlas.js
@@ -5,53 +5,10 @@ module.exports = Backbone.View.extend({
     initialize: function(element, options) {
         this.$el = $(element);
         if (options) _.extend(this, options);
-        _.bindAll(this, "emphasize", "maximize", "minimize", "minimize_all", "navigate");
-        _.bindAll(this, "loadCancerList", "initGeneTypeahead");
+        _.bindAll(this, "navigate", "loadCancerList", "initGeneTypeahead");
 
         $.ajax({ url:"svc/data/lookups/cancers", type:"GET", dataType:"text", success:this.loadCancerList });
         $.ajax({ url:"svc/data/lookups/genes", type:"GET", dataType:"text", success:this.initGeneTypeahead });
-    },
-
-    emphasize: function(options) {
-        var $step = options.$el;
-        if ($step && $step.length) {
-            this.minimize_all();
-            this.navigate($step.find(".map-contents"), $step.data("medium"));
-            _.defer(function() {
-                $step.removeClass("small");
-                $step.addClass("medium");
-            });
-        }
-    },
-
-    maximize:function (options) {
-        var $step = options.$el;
-        if ($step && $step.length) {
-            this.minimize_all();
-            this.navigate($step.find(".map-contents"), $step.data("big"));
-            _.defer(function() {
-                $step.removeClass("small");
-                $step.addClass("big");
-            });
-        }
-    },
-
-    minimize:function (options) {
-        var $step = options.$el;
-        if ($step && $step.length) {
-            $step.removeClass("medium");
-            $step.removeClass("big");
-            $step.addClass("small");
-
-            this.navigate($step.find(".map-contents"), $step.data("small"));
-        }
-    },
-
-    minimize_all: function() {
-        var _this = this;
-        _.each(this.$el.find(".atlas-map"), function(atlasMap) {
-            _this.minimize({"$el": $(atlasMap)});
-        });
     },
 
     navigate: function(target, route) {
