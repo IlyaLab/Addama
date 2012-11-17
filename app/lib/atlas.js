835cb433d9dd052c10dbebef8efe93209d64b21d
Hector Rovira <hrovira@systemsbiology.org>
2012-11-17 03:23:17 -0800

    restructuring atlas view

--- app/lib/atlas.js
+++ app/lib/atlas.js
@@ -1,4 +1,3 @@
-var QEDRouter = require("./router");
 var LineItemTemplate = require("../views/templates/line_item");
 
 module.exports = Backbone.View.extend({
@@ -11,21 +10,60 @@ module.exports = Backbone.View.extend({
         $.ajax({ url:"svc/data/lookups/genes", type:"GET", dataType:"text", success:this.initGeneTypeahead });
     },
 
-    navigate: function(target, route) {
-        if (target && route) {
-            route = route.replace("#v/", "");
-            var uri = route.substring(0, route.lastIndexOf("/"));
-            var view_name = route.substring(route.lastIndexOf("/") + 1);
-
+    navigate: function(target, source, view_name) {
+        if (target && source && view_name) {
             var afn = function(link) {
                 return $(link).data("id")
             };
-            var router = new QEDRouter({"targetEl": $(target)});
-            router.viewsByUri(uri, view_name, {
+
+            var view = this.viewsByUri($(target), source, view_name, {
                 "cancers": _.map($(".cancer-selector .active a"), afn),
                 "genes": _.map($(".gene-selector .item-remover"), afn)
             });
+            view.on("selected", function(selected) {
+                console.log("selected=" + selected.id);
+            });
         }
+    },
+
+    viewsByUri: function($el, uri, view_name, options) {
+        console.log("viewsByUri(" + uri + "," + view_name + "," + JSON.stringify(options) + ")");
+        var parts = uri.split("/");
+        var data_root = parts[0];
+        var analysis_id = parts[1];
+        var dataset_id = parts[2];
+        var model_unit = qed.Datamodel.get(data_root)[analysis_id];
+        var catalog = model_unit.catalog;
+        var catalog_unit = catalog[dataset_id];
+        var modelName = model_unit.model || catalog_unit.model;
+        var serviceUri = model_unit.service || catalog_unit.service || "data/" + uri;
+        var Model = qed.Models[modelName];
+
+        var model_optns = _.extend(options, {
+            "data_uri": "svc/" + serviceUri,
+            "analysis_id": analysis_id,
+            "dataset_id": dataset_id,
+            "model_unit": model_unit,
+            "catalog_unit": catalog_unit
+        });
+        // TODO: Determine which views need annotations
+        // qed.FetchAnnotations(dataset_id);
+
+        var model = new Model(model_optns);
+        _.defer(function() {
+            model.fetch({
+                success:function () {
+                    model.trigger('load');
+                }
+            });
+        });
+
+        var view_options = _.extend({"model":model}, (model_unit.view_options || {}), (options || {}));
+
+        var ViewClass = qed.Views[view_name];
+        var view = new ViewClass(view_options);
+        $el.html(view.render().el);
+        return view;
     },
 
     loadCancerList: function(txt) {
@@ -100,5 +138,18 @@ module.exports = Backbone.View.extend({
                 $elto.zoomTo(_.extend(defaultZoom, { "targetsize":0.9 }));
             }, 1200);
         }
+    },
+
+    zoom: function(level) {
+        // TODO : Figure out way to center screen on where the mouse has clicked
+        $(".atlas-zoom").zoomTo({
+            "duration":1000,
+            "scalemode": "both",
+            "easing": "ease",
+            "nativeanimation": true,
+            "root": $(".atlas-canvas"),
+            "closeclick": false,
+            "targetsize":level
+        });
     }
 });
