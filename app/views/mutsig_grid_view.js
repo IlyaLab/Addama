a7fa7682d1fb06e27752b768a44d9dd271a76c79
Hector Rovira <hrovira@systemsbiology.org>
2012-11-18 00:45:32 -0800

    generalizing data grid

--- app/views/mutsig_grid_view.js
+++ app/views/mutsig_grid_view.js
@@ -1,6 +1,9 @@
-var Template = require('./templates/mutsig_grid');
+var Template = require('./templates/data_grid');
 
 module.exports = Backbone.View.extend({
+    header_attribute: "cancer",
+    row_attribute: "gene",
+    cell_attribute: "rank",
 
     initialize: function (options) {
         _.extend(this, options);
@@ -11,19 +14,20 @@ module.exports = Backbone.View.extend({
 
     processData: function() {
         var items = this.model.get("items");
-        var data = _.groupBy(items, "gene");
-        var cancers = _.uniq(_.pluck(items, "cancer"));
-        var genes = _.uniq(_.pluck(items, "gene"));
-        
+        var data = _.groupBy(items, this.row_attribute);
+        var rowlabels = _.uniq(_.pluck(items, this.row_attribute));
+        var headers = _.map(_.uniq(_.pluck(items, this.header_attribute)), function(h) {
+            return { "id": h };
+        });
+
+        _.each(items, function(item) {
+            item["cell_value"] = item[this.cell_attribute];
+        }, this);
+
         this.$el.html(Template({
-            "cancers": _.map(cancers, function(c) {
-                return { "id": c };
-            }),
-            "genes": _.map(genes, function(gene) {
-                return {
-                    "id": gene,
-                    "rank_by_cancer": data[gene]
-                };
+            "headers": headers,
+            "rows": _.map(rowlabels, function(rowlabel) {
+                return { "label": rowlabel, "values": data[rowlabel] };
             })
         }));
     }
