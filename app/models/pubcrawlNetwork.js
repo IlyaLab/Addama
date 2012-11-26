04b3ae1577090697ecc3a3c472306de7eab540b6
Andrea Eakin <aeakin@systemsbiology.org>
2012-11-26 09:49:54 -0800

    adding datatables integration

--- app/models/pubcrawlNetwork.js
+++ app/models/pubcrawlNetwork.js
@@ -99,6 +99,75 @@ var NodeCollection = Backbone.Collection.extend({
     model: Node
 });
 
+NodeDetailsModel = Backbone.Model.extend({
+    //url for this model is the solr connection to retrieve documents related to this node
+    urlRoot: 'http://apollo:4080/solr/core0/select/?qt=distributed_select&sort=pub_date_year desc&wt=json&hl=true&hl.fl=article_title,abstract_text&rows=1000&hl.snippets=100&hl.fragsize=50000&h.mergeContiguous=true',
+    url: function(){
+        return this.urlRoot + "&q=+text:(" + this.nodeName + ")&fq=pub_date_year:[1991 TO 2012]";
+    },
+
+    initialize: function(networkModel,nodeName){
+        //setup the various model items by finding all edges with the nodeName and putting into the appropriate jsonarray
+        this.nodeName = nodeName;
+        this.nmdDetailsModel = [];
+        this.domineDetailsModel = [];
+        for(var i=0; i< networkModel.nodes.length; i++){
+            if(networkModel.nodes.models[i].name == nodeName){
+                this.node = networkModel.nodes.models[i];
+                break;
+            }
+        }
+        for(var i=0; i< networkModel.edges.length; i++){
+            var edge = networkModel.edges.models[i];
+            if(edge.source.node == this.node){
+                if(edge.relType == "ngd"){
+
+                    var edgeItem={name: edge.target.node.name, combocount: edge.combocount, termcount: edge.target.node.termcount,nmd:edge.nmd};
+                    this.nmdDetailsModel.push(edgeItem);
+                }
+                else if(edge.relType == "domine"){
+                    var edgeItem={term1: edge.source.node.name, term2: edge.target.node.name, pf1: edge.pf1, pf2: edge.pf2,
+                        pf1_count: edge.pf1_count, pf2_count: edge.pf2_count, type: edge.type, uni1: edge.uni1, uni2: edge.uni2};
+                    this.domineDetailsModel.push(edgeItem);
+                }
+            }
+            else if(edge.target.node == this.node){
+                //don't need to do ngd here, since it is currently doubled, should be able to also remove domine once it is doubled correctly
+                if(edge.relType == "domine"){
+                    var edgeItem={term1: edge.target.node.name, term2: edge.source.node.name, pf1: edge.pf1, pf2: edge.pf2,
+                        pf1_count: edge.pf1_count, pf2_count: edge.pf2_count, type: edge.type, uni1: edge.uni1, uni2: edge.uni2};
+                    this.domineDetailsModel.push(edgeItem);
+                }
+            }
+        }
+
+    },
+
+    parse: function(response){
+        this.docs=[];
+        if(response.response.docs != null){
+            for (var i=0; i < response.response.docs.length; i++){
+                var doc = response.response.docs[i];
+                if(response.highlighting[doc.pmid] != undefined){
+                    if(response.highlighting[doc.pmid].abstract_text != undefined){
+                        doc.abstract_text = response.highlighting[doc.pmid].abstract_text;
+                    }
+                    if(response.highlighting[doc.pmid].article_title != undefined){
+                        doc.article_title = response.highlighting[doc.pmid].article_title;
+                    }
+                }
+
+                this.docs.push(doc);
+            }
+        }
+
+        return;
+
+    }
+
+});
+
+
 var NetworkModel = Backbone.Model.extend({
          url: function(){
                return this.get("data_uri") + "/graphs/pubcrawl/relationships/query" + "?nodeSet=" + this.createNodeUrlString() + "&relationshipSet=[{name:'ngd'},{name:'domine'}]";
@@ -205,6 +274,7 @@ module.exports = Backbone.Model.extend({
                         });
                         that.selectedNodes.push(model.searchData);
                         that.networkData = new NetworkModel({data_uri: that.get("data_uri"),nodes: that.selectedNodes, searchterm: that.searchTerm});
+
                         that.networkData.fetch({success: function(model,response) {
                             options.success(model,response);
                         }});
--- app/views/pubcrawl_network.js
+++ app/views/pubcrawl_network.js
@@ -1,17 +1,196 @@
+var Template = require('../views/templates/pubcrawl');
+var NodeTabTemplate = require('../views/templates/nodeTabTable');
+var TableView = Backbone.View.extend({
+
+    initialize: function(config){
+        //model contains the data for the table
+        this.dataConfig = config.dataConfig; //contains the header data for the table['Name','Aliases','Term Single Count','Term Combo Count','NMD']
+        this.checkbox=config.checkbox;   //true if the first column should be a checkbox selection
+        this.tableId = config.tableId; //id of the table
+        this.expandedConfig = config.expandedConfig //contains the data config for any data that should show up in row details
+
+    },
+
+    render: function(){
+
+        var thisView = this;
 
 
+        //now make data table for modal window
+        var table='';
+
+
+        if(this.checkbox){
+            table='<table class="table table-striped table-bordered" width="500"  id="' + this.tableId + '">'+
+                '</table>';
+        }
+        else{
+            table ='<table class="table nocheckbox_table table-striped table-bordered" width="500"  id="' + this.tableId + '">'+
+                '</table>';
+        }
+
+        this.$el.html(table);
+        var aoColumns = [];
+        if(this.checkbox){
+            aoColumns.push( { "sSortDataType": "dom-checkbox", "sTitle": '<input id="check_all" type="checkbox"/>',
+                "sWidth": "10px","sDefaultContent":'<input class="tableCheckbox" type="checkbox" />'});
+        }
+        if(this.expandedConfig != null && this.expandedConfig.length > 0){
+            aoColumns.push( {"mDataProp": null,
+                "sWidth":"10px",
+                               "sClass": "control center",
+                                "sDefaultContent": '<img src="../img/details_open.png">'});
+        }
+        for(var k=0; k < thisView.dataConfig.length; k++){
+            var dataProp=thisView.dataConfig[k].propName;
+            if(thisView.dataConfig[k].urlLink != null){
+                   dataProp="<a href='" + thisView.dataConfig[k].urlLink + dataProp + "' target='_blank'>"+dataProp + "</a>";
+            }
+            aoColumns.push({"sTitle": thisView.dataConfig[k].headerName,"mDataProp": dataProp,
+            "sWidth":thisView.dataConfig[k].headerWidth,"sDefaultContent": ""});
+        }
+
+        this.oTable=this.$el.find("#" + this.tableId).dataTable({
+            "sDom": "<'row'<'span3'l><'span4'f>r>t<'row'<'span3'i><'span4'p>>",
+            "sPaginationType": "bootstrap",
+            "bAutoWidth":false,
+            "aaData": this.model,
+            "oLanguage": {
+                "sLengthMenu": "_MENU_ records per page"
+            },
+
+            "aoColumns":aoColumns,
+
+            "fnInitComplete"  : function () {
+                var that=this;
+                this.fnAdjustColumnSizing(true);
+                if(thisView.checkbox){
+                thisView.$el.find('#check_all').click( function() {
+                    $('input', that.fnGetNodes()).attr('checked',this.checked);
+                    thisView.$el.trigger("tableSelectionChange");
+                } );
+                this.$('.tableCheckbox').click(function(){
+                    thisView.$el.trigger("tableSelectionChange");
+                });
+                }
+                else{
+                    this.$('tbody tr').click(function(item){
+
+                    });
+                }
+
+            }
+        });
+
+        thisView.anOpen=[];
+        if(thisView.expandedConfig != null && thisView.expandedConfig.length > 0){
+            $("#" + this.tableId + " td.control").live('click', function(){
+                var nTr = this.parentNode;
+                var i = $.inArray(nTr,thisView.anOpen);
+
+                if(i == -1){  //this item was not in the open list, open it
+                    $('img', this).attr('src',"../img/details_close.png" );
+                    thisView.oTable.fnOpen(nTr, thisView.formatDetails(thisView.oTable, nTr), 'details');
+                    thisView.anOpen.push(nTr);
+                }
+                else{
+                    $('img',this).attr('src',"../img/details_open.png" );
+                    thisView.oTable.fnClose(nTr);
+                    thisView.anOpen.splice(i,1);
+                }
+            });
+        }
+
+        return this;
+    },
+
+
+
+    formatDetails: function(oTable, row){
+         var data = oTable.fnGetData(row);
+        var sOut = '<div class="innerDetails">'+
+            '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+        for(var k=0; k< this.expandedConfig.length; k++){
+            sOut+='<tr><td>' + data[this.expandedConfig[k].propName] + '</td></tr>';
+        }
+        sOut+='</table></div>';
+        return sOut;
+    }
+
+});
+
+var NodeTabTableView = Backbone.View.extend({
+
+    initialize: function(){
+
+        this.$el.html(NodeTabTemplate({}));
+        this.model.bind("reset", this.render, this);
+    },
+
+
+    render: function(){
+
+        var tabs = '<ul class="nav nav-tabs" id="nodeDetailsTabs">' +
+            '<li class="active"><a id="qfDocTab" href="#docTableView" data-toggle="tab">Medline Documents</a></li>' +
+            '<li><a id="qfNMDTab" href="#nmdTableView" data-toggle="tab">NMD Connections</a></li>' +
+            '<li><a id="qfDomineTab" href="#domineTableView" data-toggle="tab">Domine Connections</a></li>' +
+            '</ul>' +
+            '<div class="tab-content">' +
+            '<div class="tab-pane active queryfiltertable" id="docTableView"></div>' +
+            '<div class="tab-pane queryfiltertable" id="nmdTableView">' +
+            '</div>' +
+            '<div class="tab-pane queryfiltertable" id="domineTableView"></div>' +
+            '</div>';
+
+        this.$el.find("#nodeDetailsModalBody").html(tabs);
+
+        var docConfig = [{headerName:'PMID', headerWidth: '10%', propName:'pmid', urlLink:'http://www.ncbi.nlm.nih.gov/pubmed/'},
+            {headerName:'Title', headerWidth: '50%', propName: 'article_title'},
+            {headerName:'Pub. Year', headerWidth: '10%', propName: 'pub_date_year'}];
+
+        var expConfig = [{propName:'abstract_text'}];
+
+        this.docView = new TableView({dataConfig: docConfig, expandedConfig: expConfig, checkbox: false, tableId: "docTable",model: this.model.docs});
+        this.$el.find("#docTableView").html(this.docView.render().el);
+
+        var nmdConfig = [{headerName:'Name', headerWidth: '30%', propName:'name'},
+            {headerName:'Term Single Count', headerWidth: '10%', propName: 'termcount'},
+            {headerName:'Term Combo Count', headerWidth: '10%', propName: 'combocount'},
+            {headerName:'NMD', headerWidth: '10%', propName: 'nmd'}];
+
+        this.nmdView = new TableView({dataConfig: nmdConfig, checkbox: false, tableId: "nmdTable",model: this.model.nmdDetailsModel});
+        this.$el.find("#nmdTableView").html(this.nmdView.render().el);
+
+        var dataConfig = [{headerName:'Term1', headerWidth: '30%', propName:'term1'},
+            {headerName:'Term2', headerWidth: '10%', propName: 'term2'},
+            {headerName:'UniProt ID1', headerWidth: '10%', propName: 'uni1'},
+            {headerName:'UniProt ID2', headerWidth: '10%', propName: 'uni2'},
+            {headerName:'Domain 1', headerWidth: '10%', propName: 'pf1'},
+            {headerName:'Domain 2', headerWidth: '10%', propName: 'pf2'},
+            {headerName:'Type', headerWidth: '10%', propName: 'type'},
+            {headerName:'Domain 1 Count', headerWidth: '10%', propName: 'pf1_count'},
+            {headerName:'Domain 2 Count', headerWidth: '10%', propName: 'pf2_count'}];
+
+        this.domineView = new TableView({dataConfig: dataConfig, checkbox: false, tableId: "domineTable",model: this.model.domineDetailsModel});
+        this.$el.find("#domineTableView").html(this.domineView.render().el);
+
+        return this;
+    }
+});
 module.exports = Backbone.View.extend({
 
     initialize:function (options) {
         _.extend(this, options);
         _.bindAll(this, "loadData");
 
+        this.$el.html(Template({}));
+
         this.model.on("load", this.loadData);
     },
 
     loadData: function() {
 
-       this.render(600,400);
+      this.render(600,400);
 
     },
 
@@ -26,7 +205,7 @@ module.exports = Backbone.View.extend({
                         return this;
                     }
 
-        var svg = d3.select(this.el).append("svg:svg")
+        var svg = d3.select(this.$el.find("#pubcrawl-network-container").context).append("svg:svg")
                       .attr("width", width)
                       .attr("height", height);
 
@@ -141,7 +320,7 @@ module.exports = Backbone.View.extend({
       }
 
       function triggerNodeDetailsView(item){
-          $(that.el).trigger('nodeClicked',item.name);
+         that.showNodeDetails(item.name);
       }
 
       function triggerEdgeDetailsView(item){
@@ -156,6 +335,14 @@ module.exports = Backbone.View.extend({
                    }
 
         return this;
-     }
+     },
+
+   showNodeDetails: function(node){
+       this.nodeDetails = new NodeDetailsModel(this.model.networkData,node);
+       var that=this;
+       var datatable=this.nodeDetails.fetch({success: function(model,response){
+           $("#pubcrawl-table-container").html(new NodeTabTableView({model: model}).render().el);
+       }});
+   }
 
 });
