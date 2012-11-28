d525a4b54a61116e6712801e414c2eb70363a541
hrovira <hrovira@systemsbiology.org>
2012-11-27 16:22:19 -0800

    strip out uri and id

--- app/views/items_grid_view.js
+++ app/views/items_grid_view.js
@@ -2,23 +2,22 @@ var Template = require("./templates/items_grid");
 
 module.exports = Backbone.View.extend({
 
-    initialize: function (options) {
+    initialize:function (options) {
         _.extend(this, options);
         _.bindAll(this, "processData");
 
         this.model.on("load", this.processData);
     },
 
-    processData: function() {
+    processData:function () {
         var items = this.model.get("items");
-        var headers = _.keys(items[0]);
-        var rows = _.map(items, function(item) {
-            return { "values": _.values(item) };
+        var headers = _.without(_.keys(items[0]), "uri", "id");
+        var rows = _.map(items, function (item) {
+            return { "values":_.map(headers, function (header) {
+                return item[header];
+            })};
         });
 
-        this.$el.html(Template({
-            "headers": headers,
-            "rows": rows
-        }));
+        this.$el.html(Template({ "headers":headers, "rows":rows }));
     }
 });
