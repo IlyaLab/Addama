79028584abaa112c17b2175d43741463e9add351
Hector Rovira <hrovira@systemsbiology.org>
2012-12-01 11:33:12 -0800

    script to sync local version of qed_atlas.json and qed_datamodel.json

---
+++ misc/sync_qed_configs.sh
@@ -0,0 +1,4 @@
+curl -O http://genespot.cancerregulome.org/svc/data/lookups/chromosomes
+curl -O http://genespot.cancerregulome.org/svc/data/qed_atlas.json 
+curl -O http://genespot.cancerregulome.org/svc/data/qed_datamodel.json 
+
