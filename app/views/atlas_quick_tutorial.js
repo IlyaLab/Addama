8e0f0711b30dedf9b0a7203245c71e255ae08e54
Hector Rovira <hrovira@systemsbiology.org>
2012-12-03 00:27:50 -0800

    added quick tutorial

---
+++ app/views/atlas_quick_tutorial.js
@@ -0,0 +1,8 @@
+Template = require("../views/templates/atlas_quick_tutorial");
+
+module.exports = Backbone.View.extend({
+    initialize: function(options) {
+        var _id = Math.round(Math.random() * 100000);
+        this.$el.html(Template({"id":_id}));
+    }
+});
