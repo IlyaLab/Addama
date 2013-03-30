e9e6502363ad69f91382cf903578254cb0edd544
Hector Rovira <hrovira@systemsbiology.org>
2013-03-29 17:56:53 -0700

    initial layout

--- app/views/minigraph.js
+++ app/views/minigraph.js
@@ -1,18 +1,40 @@
+var Template = require("../templates/minigraph");
+
 module.exports = Backbone.View.extend({
-    initialize: function() {
+    initialize: function () {
         console.log("minigraph.view.initialize:" + JSON.stringify(this.options));
 
-        _.bindAll(this, "renderHello");
+        _.bindAll(this, "renderData");
 
-        this.model.on("load", this.renderHello);
+        this.model.on("load", this.renderData);
     },
 
-    render: function() {
+    render: function () {
         this.$el.html("minigraph on!");
         return this;
     },
 
-    renderHello: function() {
-        this.$el.html("minigraph hello!");
+    renderData: function () {
+        var nodes = this.model.get("nodes");
+        var grouped = _.groupBy(nodes, "type");
+        var nodetypes = _.map(grouped, function (group, type) {
+            return {
+
+                "label": type,
+                "nodes": _.map(group, function (item) {
+                    var keys = _.without(_.keys(item), "id", "type");
+                    return {
+                        "label": item["id"],
+                        "measures": _.map(keys, function (key) {
+                            return {
+                                "label": key,
+                                "value": item[key]
+                            }
+                        })
+                    };
+                })
+            }
+        });
+        this.$el.html(Template({ "nodetypes": nodetypes }));
     }
 });
