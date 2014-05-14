2e1a34bd53bfe0d77c689bde6cc890621b97461b
Dick Kreisberg <rkreisberg@systemsbiology.org>
2014-05-13 17:36:43 -0700

    more error handling

--- tasks/celeryHandlers.py
+++ tasks/celeryHandlers.py
@@ -6,7 +6,7 @@ from tornado.options import options
 
 from oauth.decorator import CheckAuthorized
 
-from celeryTasks import CeleryTasks
+import celeryTasks
 
 import tcelery
 tcelery.setup_nonblocking_producer()
@@ -14,46 +14,56 @@ tcelery.setup_nonblocking_producer()
 class TaskHandler(RequestHandler):
 
     tasks_map = {
-    "pairwise" : CeleryTasks.justInTimePairwise
+    "pairwise" : celeryTasks.justInTimePairwise
     }
   
     @asynchronous
     @CheckAuthorized
     def get(self, *uri_path):
-        if options.verbose: logging.info("GET [uri=%s] body=%s]" % (self.request.uri, self.request.body))
-
-        sub_path = self.request.path.replace("/tasks", "")
-        uri_parts = sub_path.split("/")
-        if options.verbose: logging.info("GET [sub_path=%s] [len=%d]" % (sub_path, len(uri_parts)))
-
-        if len(uri_parts) == 1:
-            self.list_tasks()
-            self.set_status(200)
-            return
-
-    def post(self, *uri_path):
-        if options.verbose: logging.info("GET [uri=%s] body=%s]" % (self.request.uri, self.request.body))
-
-        sub_path = self.request.path.replace("/tasks", "")
-        uri_parts = sub_path.split("/")
-        if options.verbose: logging.info("GET [sub_path=%s] [len=%d]" % (sub_path, len(uri_parts)))
-
-        if len(uri_parts) == 1:
-            self.list_tasks()
-            self.set_status(200)
-            return
-
-        task_id = uri_parts[1]
-        if not task_id in self.tasks_map.keys():
-            if options.verbose: logging.info("unknown task [%s]" % task_id)
-            raise tornado.web.HTTPError(404, ("task %s not found" % task_id))
-
         try:
-           query = tornado.escape.json_decode(self.request.body)
-           self.tasks_map[task_id].apply_async(args=query, callback=self.on_result)
+            if options.verbose: logging.info("GET [uri=%s] body=%s]" % (self.request.uri, self.request.body))
+
+            sub_path = self.request.path.replace("/tasks", "")
+            uri_parts = sub_path.split("/")
+            if options.verbose: logging.info("GET [sub_path=%s] [len=%d]" % (sub_path, len(uri_parts)))
+
+            if len(uri_parts) == 1:
+                self.list_tasks()
+                self.set_status(200)
+                return
+            raise tornado.web.HTTPError(404, ("%s was not found" % self.request.path))
         except:
+            if options.verbose: logging.info("Exception raised on /tasks")
             raise tornado.web.HTTPError(500, "Error executing pairwise computation.")
 
+    def post(self, *uri_path):
+        try:
+            if options.verbose: logging.info("GET [uri=%s] body=%s]" % (self.request.uri, self.request.body))
+
+            sub_path = self.request.path.replace("/tasks", "")
+            uri_parts = sub_path.split("/")
+            if options.verbose: logging.info("GET [sub_path=%s] [len=%d]" % (sub_path, len(uri_parts)))
+
+            if len(uri_parts) == 1:
+                self.list_tasks()
+                self.set_status(200)
+                return
+
+            task_id = uri_parts[1]
+            if not task_id in self.tasks_map.keys():
+                if options.verbose: logging.info("unknown task [%s]" % task_id)
+                raise tornado.web.HTTPError(404, ("task %s not found" % task_id))
+
+            try:
+               query = tornado.escape.json_decode(self.request.body)
+               self.tasks_map[task_id].apply_async(args=query, callback=self.on_result)
+            except:
+                raise tornado.web.HTTPError(500, "Error executing pairwise computation.")
+            
+            raise tornado.web.HTTPError(404, ("%s was not found" % self.request.path))
+        except:
+            if options.verbose: logging.info("Exception raised on /tasks")
+            raise tornado.web.HTTPError(500, "Error executing pairwise computation.")
     def on_result(self, response):
         self.write(str(response.result))
         self.finish()
